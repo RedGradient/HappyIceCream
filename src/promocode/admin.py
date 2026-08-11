fe8d3c250a914fafdb126cff636c4b51fe42e7b1
redgradient <neutral1shade@yahoo.com>
2026-08-11 12:09:15 +0400

    feature(model): add UserPromocode model

--- src/promocode/admin.py
+++ src/promocode/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
 
-from promocode.models import PromoCode
+from promocode.models import PromoCode, UserPromocode
 
 
 @admin.register(PromoCode)
 class PromoCodeAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(UserPromocode)
+class UserPromocodeAdmin(ModelAdmin):
     pass
